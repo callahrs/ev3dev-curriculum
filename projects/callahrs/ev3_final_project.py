9aa174f9964b1d1f6e70dbb8b0a8d12c7c2d8dea
callahrs <callahrs@rose-hulman.edu>
2018-08-09 15:50:45 -0400

    Progress Step

--- projects/callahrs/ev3_final_project.py
+++ projects/callahrs/ev3_final_project.py
@@ -4,9 +4,28 @@ import math
 import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
-
+import robot_controller as robo
 
 class MyDelegate(object):
 
     def __init__(self):
         self.running = True
+
+    def draw_star(self, radius, points, speed):
+        robot = robo.Snatch3r()
+        if points % 2 == 1:
+            angle = points // 180
+            turn_angle = 360 - angle
+            lenth_chord = 2 * radius * (math.sin(((180 - (2 * angle)) * (math.pi // 180)) // 2))
+            for k in range(points):
+                robot.drive_inches(lenth_chord, speed)
+                robot.turn_degrees(turn_angle, speed)
+        elif points % 2 == 0:
+            turn_angle = 180
+            turn_angle_inner = 360 - (360 // points)
+            lenth_chord = radius
+            for k in range(points):
+                robot.drive_inches(lenth_chord, speed)
+                robot.turn_degrees(turn_angle, speed)
+                robot.drive_inches(lenth_chord, speed)
+                robot.turn_degrees(turn_angle_inner, speed)
