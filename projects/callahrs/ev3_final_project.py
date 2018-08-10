5382427027f0b74d9c357ad35abca717e39c22ed
callahrs <callahrs@rose-hulman.edu>
2018-08-09 21:25:00 -0400

    Progress Step

--- projects/callahrs/ev3_final_project.py
+++ projects/callahrs/ev3_final_project.py
@@ -2,6 +2,7 @@ import time
 import math
 import mqtt_remote_method_calls as com
 import robot_controller as robo
+import ev3dev.ev3 as ev3
 
 
 class MyDelegate(object):
@@ -18,7 +19,9 @@ class MyDelegate(object):
             length_chord = 2 * radius * (math.sin(((180 - (2 * angle)) * (math.pi // 180)) // 2))
             for k in range(points):
                 robot.drive_inches_star(length_chord, speed)
+                ev3.Sound.beep().wait()
                 robot.turn_degrees(turn_angle, speed)
+                ev3.Sound.beep().wait()
                 self.count_done = self.count_done + 1
         elif points % 2 == 0:
             turn_angle = 180
@@ -26,9 +29,13 @@ class MyDelegate(object):
             length_chord = radius
             for k in range(points):
                 robot.drive_inches_star(length_chord, speed)
+                ev3.Sound.beep().wait()
                 robot.turn_degrees(turn_angle, speed)
+                ev3.Sound.beep().wait()
                 robot.drive_inches_star(length_chord, speed)
+                ev3.Sound.beep().wait()
                 robot.turn_degrees(turn_angle_inner, speed)
+                ev3.Sound.beep().wait()
                 self.count_done = self.count_done + 1
 
 
@@ -37,8 +44,9 @@ def main():
     mqtt_client = com.MqttClient(my_delegate)
     my_delegate.mqtt_client = mqtt_client
     mqtt_client.connect_to_pc()
-    mqtt_client.send_message("lines_done", [my_delegate.count_done])
-    time.sleep(0.1)
+    while my_delegate.running is True:
+        mqtt_client.send_message("lines_done", [my_delegate.count_done])
+        time.sleep(0.1)
 
 
 main()
