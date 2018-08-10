07f0b43bbdb4ad1c3da56dded7b7ed7427536b14
callahrs <callahrs@rose-hulman.edu>
2018-08-10 00:03:38 -0400

    Progress Step

--- projects/callahrs/ev3_final_project.py
+++ projects/callahrs/ev3_final_project.py
@@ -9,24 +9,23 @@ class MyDelegate(object):
 
     def __init__(self):
         self.running = True
+        self.mqtt_client = None
         self.count_done = 0
+
+    def loop_forever(self):
+        btn = ev3.Button()
+        self.running = True
+        while not btn.backspace and self.running:
+            # Do nothing while waiting for commands
+            time.sleep(0.01)
+        self.mqtt_client.close()
+        # Copied from robot.shutdown
+        print("Goodbye")
+        ev3.Sound.speak("Goodbye").wait()
 
     def draw_star(self, radius, points, speed):
         robot = robo.Snatch3r()
-        if points % 2 == 1:
-            angle = points // 180
-            print(angle)
-            length_chord = 2 * radius * (math.sin(((180 - (2 * angle)) * (math.pi // 180)) // 2))
-            print(angle)
-            for k in range(points):
-                robot.drive_inches_star(length_chord, speed)
-                ev3.Sound.beep().wait()
-                robot.turn_degrees((-1 * angle), speed)
-                ev3.Sound.beep().wait()
-                time.sleep(.2)
-                self.count_done = self.count_done + 1
-            self.running = False
-        elif points % 2 == 0:
+        if points % 2 == 0:
             turn_angle = 180
             turn_angle_inner = (360 // points)
             length_chord = radius
@@ -40,8 +39,23 @@ class MyDelegate(object):
                 robot.turn_degrees(turn_angle, speed)
                 ev3.Sound.beep().wait()
                 self.count_done = self.count_done + 1
+                self.mqtt_client.send_message("lines_done", [self.count_done])
                 time.sleep(.2)
-            self.running = False
+            self.count_done = 0
+        else:
+            angle = 180 // points
+            print(angle)
+            length_chord = 2 * radius * (math.sin(((180 - (2 * angle)) // 2)))
+            print(length_chord)
+            for k in range(points):
+                robot.drive_inches_star(length_chord, speed)
+                ev3.Sound.beep().wait()
+                robot.turn_degrees((-1 * angle), speed)
+                ev3.Sound.beep().wait()
+                time.sleep(.2)
+                self.count_done = self.count_done + 1
+                self.mqtt_client.send_message("lines_done", [self.count_done])
+            self.count_done = 0
 
 
 def main():
@@ -51,11 +65,7 @@ def main():
     mqtt_client.connect_to_pc()
     robot = robo.Snatch3r()
     robot.arm_calibration()
-    my_delegate.running = True
-    while my_delegate.running is True:
-        print("Sending " + str(my_delegate.count_done))
-        mqtt_client.send_message("lines_done", [my_delegate.count_done])
-        time.sleep(0.1)
+    my_delegate.loop_forever()
 
 
 main()
